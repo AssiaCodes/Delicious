6ed56a2a03365cfe095ffee6920df3753cb203e7
Assia Chabal <Chabalchabal00@gmail.com>
2025-05-27 17:02:10 -0500

    My second project

--- src/main/java/com/pluralsight/ReceiptWriter.java
+++ src/main/java/com/pluralsight/ReceiptWriter.java
@@ -1,4 +1,18 @@
 package com.pluralsight;
 
+import java.io.FileWriter;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class ReceiptWriter {
+
+    public static void createReceiptFile(String orderDetails) throws IOException {
+        Date now = new Date();
+        SimpleDateFormat formatter = new SimpleDateFormat("yyyyMMdd-HHmmss");
+        String formattedDate = formatter.format(now);
+        FileWriter fileWriter = new FileWriter("c://receipts//"+formattedDate+".txt");
+        fileWriter.write(orderDetails);
+        fileWriter.close();
+    }
 }
--- src/main/java/com/pluralsight/UserInterface.java
+++ src/main/java/com/pluralsight/UserInterface.java
@@ -1,4 +1,70 @@
-package com.pluralsight;
+package ui;
+
+
+import com.pluralsight.*;
+
+import java.io.IOException;
+import java.util.*;
 
 public class UserInterface {
+    private static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        while (true) {
+            System.out.println("=== DELI-cious Point of Sale ===");
+            System.out.println("1) New Order");
+            System.out.println("0) Exit");
+            System.out.print("Choose an option: ");
+            String choice = scanner.nextLine();
+
+            if (choice.equals("1")) {
+                handleNewOrder();
+            } else if (choice.equals("0")) {
+                System.out.println("Goodbye!");
+                break;
+            }
+        }
+    }
+
+    private static void handleNewOrder() throws IOException {
+        Order order = new Order();
+        while (true) {
+            System.out.println("=== Order Menu ===");
+            System.out.println("1) Add Sandwich");
+            System.out.println("2) Add Drink");
+            System.out.println("3) Add Chips");
+            System.out.println("4) Checkout");
+            System.out.println("0) Cancel Order");
+            System.out.print("Choose an option: ");
+            String choice = scanner.nextLine();
+
+            switch (choice) {
+                case "1":
+                    Sandwich sandwich = Sandwich.buildSandwich(scanner);
+                    order.addSandwich(sandwich);
+                    break;
+
+                case "2":
+                    Drink drink = Drink.orderDrink(scanner);
+                    order.addDrink(drink);
+                    break;
+
+                case "3":
+                    Chips chips = Chips.orderChips(scanner);
+                    order.addChips(chips);
+                    break;
+
+                case "4":
+                    System.out.println(order.getOrderSummary());
+                    ReceiptWriter.createReceiptFile(order.getOrderSummary());
+                    System.out.println("Total: $" + order.getTotalPrice());
+                    break;
+
+                case "0":
+                    System.out.println("Order cancelled.");
+                    return;
+            }
+        }
+    }
 }
+
